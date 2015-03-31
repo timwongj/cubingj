747a69cbc9483ec4b91bf7883725eca0d8c399a9
timothywong24 <timothywong7@gmail.com>
2015-03-31 00:52:20 -0400

    trying to set up express again

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.listen(port, ipaddress);
+app.listen(port, ip);
 
 //#!/bin/env node
 ////  OpenShift sample Node application
