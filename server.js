7274ab055d7ece93e5f423d5f5962e01a2017ee9
timothywong24 <timothywong7@gmail.com>
2015-03-31 01:21:08 -0400

    dose this work too?

--- server.js
+++ server.js
@@ -30,64 +30,64 @@ var User = mongoose.model('User', new Schema ({
     password: String
 }));
 
-//app.post('/signUp', function(req, res) {
-//    if (req.session && req.session.user)
-//        res.json({status:'yw', result:req.session.user});
-//    else {
-//        var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-//        var user = new User({
-//            firstName:req.body.firstName,
-//            lastName:req.body.lastName,
-//            email:req.body.email,
-//            password:hash
-//        });
-//        user.save(function(err) {
-//            if (err)
-//                res.json({status:'bj', result:err});
-//            else
-//                res.json({status:'gj', result:user});
-//        });
-//    }
-//});
-//
-//app.post('/login', function(req, res) {
-//    if (req.session && req.session.user)
-//        res.json({status:'yw', result:req.session.user});
-//    else {
-//        User.findOne({email: req.body.email}, function (err, user) {
-//            console.log('Found email... verifying password');
-//            if (!user) {
-//                res.json({status:'bj', result:err});
-//                console.log('User does not exist');
-//            }
-//            else if (bcrypt.compareSync(req.body.password, user.password)) {
-//                req.session.user = user;
-//                res.json({status:'gj', result:user});
-//                console.log('Logged in');
-//            }
-//            else {
-//                res.json({status:'bj', result:err});
-//                console.log('BJ');
-//            }
-//        });
-//    }
-//});
-//
-//app.get('/profile', function(req, res) {
-//    if (req.session && req.session.user) {
-//        User.findOne({email: req.session.user.email}, function (err, user) {
-//            if (user) {
-//                res.json({status:'gj', result:user});
-//            } else {
-//                req.session.reset();
-//                res.json({status:'bj', result:err});
-//            }
-//        });
-//    } else {
-//        res.json({status:'bj', result:null});
-//        console.log('Session does not exist');
-//    }
-//});
+app.post('/signUp', function(req, res) {
+    if (req.session && req.session.user)
+        res.json({status:'yw', result:req.session.user});
+    else {
+        var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+        var user = new User({
+            firstName:req.body.firstName,
+            lastName:req.body.lastName,
+            email:req.body.email,
+            password:hash
+        });
+        user.save(function(err) {
+            if (err)
+                res.json({status:'bj', result:err});
+            else
+                res.json({status:'gj', result:user});
+        });
+    }
+});
+
+app.post('/login', function(req, res) {
+    if (req.session && req.session.user)
+        res.json({status:'yw', result:req.session.user});
+    else {
+        User.findOne({email: req.body.email}, function (err, user) {
+            console.log('Found email... verifying password');
+            if (!user) {
+                res.json({status:'bj', result:err});
+                console.log('User does not exist');
+            }
+            else if (bcrypt.compareSync(req.body.password, user.password)) {
+                req.session.user = user;
+                res.json({status:'gj', result:user});
+                console.log('Logged in');
+            }
+            else {
+                res.json({status:'bj', result:err});
+                console.log('BJ');
+            }
+        });
+    }
+});
+
+app.get('/profile', function(req, res) {
+    if (req.session && req.session.user) {
+        User.findOne({email: req.session.user.email}, function (err, user) {
+            if (user) {
+                res.json({status:'gj', result:user});
+            } else {
+                req.session.reset();
+                res.json({status:'bj', result:err});
+            }
+        });
+    } else {
+        res.json({status:'bj', result:null});
+        console.log('Session does not exist');
+    }
+});
 
 app.post('/logout', function(req, res) {
     req.session.reset();
